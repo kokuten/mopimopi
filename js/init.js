1c4498a24fb90d9943fedf988600e1ba069b1076
kokuten <37765975+kokuten@users.noreply.github.com>
2021-01-20 19:25:24 +0900

    Update init.js

--- js/init.js
+++ js/init.js
@@ -623,7 +623,7 @@ Mopi2 = {
             HPS: 0        
         },
         Percentile: {
-            tt: "Percentile",
+            tt: "Pct",
             width: 40,
             padding: 0,
             alignHeader: "center",
